1a33940bd1731ee8209021ac0dc32b1ff5076ce7
Adis773 <adis.abenov@mail.ru>
2025-03-01 19:10:53 +0500

    Create Config.js

---
+++ Config.js
@@ -0,0 +1,8 @@
+// Файл конфигурации: укажите свои API-ключи и URL-адреса.
+// Не публикуйте реальные ключи в публичном репозитории!
+const CONFIG = {
+  CHATGPT_API_URL: "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",
+  OCR_API_URL: "https://api.ocr.space/parse/image",
+  OCR_API_KEY: "K89237056288957",  // если требуется
+  TRANSLATE_API_URL: "https://translate.astian.org/translate"
+};
